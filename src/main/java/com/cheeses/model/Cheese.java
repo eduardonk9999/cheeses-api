ed5cb231d83d0192cb82eecc50e70b17ebff225f
eduardonk9999 <thiago.novak7@gmail.com>
2023-09-15 17:32:28 -0300

    cheese

--- src/main/java/com/cheeses/model/Cheese.java
+++ src/main/java/com/cheeses/model/Cheese.java
@@ -1,5 +1,11 @@
 package com.cheeses.model;
 
+import javax.annotation.Generated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Cheese {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     
 }
