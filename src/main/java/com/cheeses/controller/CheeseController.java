56e5e101af7f09827180b90f56bd67e09a0535ca
eduardonk9999 <thiago.novak7@gmail.com>
2023-09-16 10:34:35 -0300

    feat: class cheeseController

--- src/main/java/com/cheeses/controller/CheeseController.java
+++ src/main/java/com/cheeses/controller/CheeseController.java
@@ -1,5 +1,46 @@
 package com.cheeses.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+import com.cheeses.model.Cheese;
+import com.cheeses.repository.CheeseRepository;
+
+@RestController
+@RequestMapping("/cheeses")
 public class CheeseController {
     
+    // Injeção de dependência, isso vem lá do repositório cheese
+    @Autowired
+    private CheeseRepository cheeseRepository;
+
+    // método para listar os nossos queijos
+    @GetMapping("/")
+    public List<Cheese> listCheeses(){
+        return(List<Cheese>) cheeseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Cheese> getCheese(@PathVariable Long id) {
+        return cheeseRepository.findById(id);
+    }
+
+    @PostMapping("/")
+    public void addCheese(@RequestBody Cheese cheese) {
+        cheeseRepository.save(cheese);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCheese(@PathVariable Long id, @RequestBody Cheese cheese) {
+        cheese.setId(id);
+        cheeseRepository.save(cheese);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCheese(@PathVariable Long id) {
+        cheeseRepository.deleteById(id);
+    }
+
 }
--- src/main/java/com/cheeses/repository/CheeseRepository.java
+++ src/main/java/com/cheeses/repository/CheeseRepository.java
@@ -1,5 +1,21 @@
 package com.cheeses.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.cheeses.model.Cheese;
+
 public class CheeseRepository {
+
+    public List<Cheese> findAll() {
+        return null;
+    }
+
+    public Optional<Cheese> findById(Long id) {
+        return null;
+    }
+
+    public void save(Cheese cheese) {
+    }
     
 }
